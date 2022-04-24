51ced93ca61e31d211756fcf566050b44e25b0a7
Giovana Silva <85005281+giovansilva@users.noreply.github.com>
2022-04-24 17:54:05 -0300

    add html exercises

---
+++ html - css/exercícios/testes pessoais/palhaçadinha/julgadordenome.py
@@ -0,0 +1,14 @@
+print('-----'*5)
+print('JULGADOR DE NOMES')
+print('-----'*5)
+nome = str(input('Qual é o seu nome?')).strip().capitalize()
+if nome == 'Noah':
+    print('Que nome lindo (✿ ♡‿♡)')
+elif nome in "Nate Gabriel Leite":
+    print('Acho que você tem uma namorada muito linda {} ♥╣[-_-]╠♥'.format(nome))
+elif nome in 'Leandro Lizandro Hittler Bolsonaro Jair Whindersson':
+    print('VAI TOMAR NO SEU CU╭∩╮ಠ_ಠ╭∩╮')
+elif nome in 'Ana Maria CLáudia Pedro Paulo Miguel Lucy Débora':
+    print('Que nome popular amg :/')
+else:
+    print('Você tem um nome muito normal¯\_(ツ)_/¯')
